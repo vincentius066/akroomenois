91babf2db1628bf2e4946a696b9811334fea8a1b
Vincentius <vincentdesr2@gmail.com>
2026-06-22 15:10:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // ==========================================
   const interfaceHTML = `
     <div id="topBar">
-      <button id="homeBtn">🏠</button>
+      <button id="homeBtn">🏠🏠</button>
       <div id="title">${document.title}</div> <button id="settingsBtn">⚙️</button>
     </div>
 
