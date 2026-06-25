587a864c2741956876135343da9ae8ecf05fbfb0
Vincentius <vincentdesr2@gmail.com>
2026-06-25 11:18:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -262,10 +262,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Greek Numeral Timeline Track
   function convertToGreekNumerals(num) {
-    if (num === 0) return 'ō'; // Fallback for 00 minutes/seconds
+    if (num === 0) return ' 𐆊'; // Fallback for 00 minutes/seconds
     
-    const tens = ['', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ϙ'];
-    const ones = ['', 'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ϛ', 'Ζ', 'Η', 'Θ'];
+    const tens = [' ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ϙ'];
+    const ones = [' ', 'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ϛ', 'Ζ', 'Η', 'Θ'];
     
     let result = '';
     
