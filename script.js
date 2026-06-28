17f2fe203291606527527603fd833f4bba47a7ac
Vincentius <vincentdesr2@gmail.com>
2026-06-28 17:31:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const interfaceHTML = `
     <div id="topBar">
       <button id="homeBtn"><img src="icon/arrow-left.svg" alt="Play" width="32" height="32"></button>
-      <div id="title">${document.title}(test 5)</div>
+      <div id="title">${document.title}(test 6)</div>
       <div id="moreMenuWrapper" style="display: flex; align-items: center; flex-direction: row;">
         <div id="extraActionsGroup" style="display: none; align-items: center; gap: 10px; margin-right: 10px;">
           <button id="freqBtn" title="Word Frequency" style="cursor: pointer; z-index: 10;"><img src="icon/insights.svg" alt="Settings" width="32" height="32"></button>
@@ -827,7 +827,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const savedPiStyle = localStorage.getItem("reader_piStyle") || "standard";
     piStyleControl.value = savedPiStyle;
     if (savedPiStyle === "cursive") {
-      updateDocumentSigmaStyle("cursive");
+      updateDocumentPiStyle("cursive");
     }
 
     piStyleControl.addEventListener("change", () => {
