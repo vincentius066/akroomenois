a21c541c77c8eacf718c70a3c7812ca9e20c6ee1
Vincentius <vincentdesr2@gmail.com>
2026-08-06 18:14:13 -0400

    Update logout function to handle multiple Set-Cookie headers

--- functions/auth/logout.js
+++ functions/auth/logout.js
@@ -1,7 +1,11 @@
 export function onRequest(context) {
   return Response.redirect('https://akroomenois.com/home', 302, {
     headers: {
-      'Set-Cookie': 'patreon_session=; Path=/; Domain=akroomenois.com; Max-Age=0; Secure;',
+      'Set-Cookie': [
+        'patreon_session=; Path=/; Max-Age=0; Secure; SameSite=Lax',
+        'patreon_session=; Domain=akroomenois.com; Path=/; Max-Age=0; Secure; SameSite=Lax',
+        'patreon_session=; Domain=.akroomenois.com; Path=/; Max-Age=0; Secure; SameSite=Lax'
+      ],
       'Cache-Control': 'no-cache, no-store, must-revalidate'
     }
   });
