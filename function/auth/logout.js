90d8a28f89a30f44b3d95ff3ee0dae768dabb95c
Vincentius <vincentdesr2@gmail.com>
2026-08-06 16:35:40 -0400

    Refactor logout function by removing comment
    Removed comment about clearing the cookie.

--- function/auth/logout.js
+++ function/auth/logout.js
@@ -1,7 +1,6 @@
 // functions/auth/logout.js
 
 export function onRequest(context) {
-    // Clear the cookie and redirect to /home
     return Response.redirect('https://akroomenois.com/home', 302, {
         headers: {
             'Set-Cookie': 'patreon_session=; Path=/; Domain=akroomenois.com; Max-Age=0; Secure; HttpOnly; SameSite=Lax',
